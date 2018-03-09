102407fc392f2ef7511e4a789bdf2394b3b9f4ed
Administrator <admin@example.com>
2018-03-08 16:36:42 -0800

    try

---
+++ grails-app/controllers/helloworld/HelloController.groovy
@@ -0,0 +1,11 @@
+package helloworld
+
+class HelloController {
+
+    def index() {
+        render "HelloWorld"
+    }
+    def hello() {
+        render "hello1"
+    }
+}
---
+++ test/unit/helloworld/HelloControllerSpec.groovy
@@ -0,0 +1,25 @@
+package helloworld
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(HelloController)
+class HelloControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test hello"() {
+        when:
+        controller.hello()
+
+        then:
+        response.text == 'hello'
+    }
+}
